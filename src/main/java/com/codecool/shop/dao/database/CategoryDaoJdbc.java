74cb8cc69f445dd585ec000a2928c7fc34d8c6ab
TudorDan <tudor.ist@gmail.com>
2020-08-18 21:19:04 +0300

    Make CategoryDaoJdbc singleton and add DatabaseManager member

--- src/main/java/com/codecool/shop/dao/database/CategoryDaoJdbc.java
+++ src/main/java/com/codecool/shop/dao/database/CategoryDaoJdbc.java
@@ -1,11 +1,27 @@
 package com.codecool.shop.dao.database;
 
 import com.codecool.shop.dao.CategoryDao;
+import com.codecool.shop.dao.DataStore;
 import com.codecool.shop.model.Category;
 
 import java.util.List;
 
 public class CategoryDaoJdbc implements CategoryDao {
+    private static CategoryDaoJdbc instance;
+    private final DatabaseManager databaseManager;
+
+    private CategoryDaoJdbc() {
+        DataStore dataStore = DataStore.getInstance();
+        this.databaseManager = dataStore.getDatabaseManager();
+    }
+
+    public static CategoryDaoJdbc getInstance() {
+        if(instance == null) {
+            instance = new CategoryDaoJdbc();
+        }
+        return instance;
+    }
+
     @Override
     public void add(Category category) {
         // TODO: 18.08.2020 add(Category)
