0bb384ebe7edd2e1ad91412119332d5b2abd322f
TudorDan <tudor.ist@gmail.com>
2020-08-18 19:08:21 +0300

    Add CategoryDaoJdbc (incomplete)

---
+++ src/main/java/com/codecool/shop/dao/database/CategoryDaoJdbc.java
@@ -0,0 +1,28 @@
+package com.codecool.shop.dao.database;
+
+import com.codecool.shop.dao.CategoryDao;
+import com.codecool.shop.model.Category;
+
+import java.util.List;
+
+public class CategoryDaoJdbc implements CategoryDao {
+    @Override
+    public void add(Category category) {
+
+    }
+
+    @Override
+    public Category find(int id) {
+        return null;
+    }
+
+    @Override
+    public void remove(int id) {
+
+    }
+
+    @Override
+    public List<Category> getAll() {
+        return null;
+    }
+}
