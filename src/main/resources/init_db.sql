f50de46bd3d8850bac47adb9b86739c4ab5d65ef
TudorDan <tudor.ist@gmail.com>
2020-08-19 22:58:33 +0300

    Redesign database to better reflect data structure

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -21,13 +21,12 @@ CREATE TABLE products
 (
     id              SERIAL PRIMARY KEY NOT NULL,
     supplier_id     INTEGER REFERENCES suppliers (id) ON DELETE CASCADE,
-    category_id     INTEGER REFERENCES categories (id) ON DELETE CASCADE,
+    category_id     INTEGER REFERENCES categories (id) ON DELETE SET NULL,
     name            VARCHAR(255),
     description     TEXT,
     image_file_name VARCHAR(255)
 );
 
--- additional tables (mirroring the code)
 DROP TABLE IF EXISTS prices CASCADE;
 CREATE TABLE prices
 (
@@ -48,15 +47,9 @@ CREATE TABLE addresses
     home_address    VARCHAR(100)
 );
 
-DROP TABLE IF EXISTS carts CASCADE;
-CREATE TABLE carts
-(
-    id  SERIAL PRIMARY KEY NOT NULL
-);
-
 DROP TABLE IF EXISTS users CASCADE;
-DROP TYPE IF EXISTS userStatus;
-CREATE TYPE userStatus AS ENUM('SIGNED', 'UNSIGNED');
+DROP TYPE IF EXISTS UserStatus;
+CREATE TYPE UserStatus AS ENUM('SIGNED', 'UNSIGNED');
 CREATE TABLE users
 (
     id              SERIAL PRIMARY KEY NOT NULL,
@@ -66,24 +59,29 @@ CREATE TABLE users
     phone_number    VARCHAR(30),
     billing_id      INTEGER REFERENCES addresses (id) ON DELETE SET NULL,
     shipping_id     INTEGER REFERENCES addresses (id) ON DELETE SET NULL,
-    user_status     userStatus,
-    my_cart_id      INTEGER REFERENCES carts (id) NOT NULL
+    user_status     UserStatus
+);
+
+DROP TABLE IF EXISTS orders CASCADE;
+DROP TYPE IF EXISTS OrderStatus;
+CREATE TYPE OrderStatus AS ENUM('CHECKED', 'PAID', 'CONFIRMED', 'SHIPPED');
+CREATE TABLE orders
+(
+    id              SERIAL PRIMARY KEY NOT NULL,
+    name            VARCHAR(50),
+    user_id         INTEGER REFERENCES users (id) ON DELETE CASCADE,
+    is_my_cart      BOOLEAN NOT NULL,
+    date            DATE,
+    order_status    OrderStatus
 );
 
 DROP TABLE IF EXISTS items CASCADE;
 CREATE TABLE items
 (
     id          SERIAL PRIMARY KEY NOT NULL,
-    product_id  INTEGER REFERENCES products (id) ON DELETE SET NULL,
     price_id    INTEGER REFERENCES prices (id) ON DELETE SET NULL,
-    cart_id     INTEGER REFERENCES carts (id) ON DELETE CASCADE,
+    order_id    INTEGER REFERENCES orders (id) ON DELETE CASCADE,
     quantity    INTEGER
-);
-
-DROP TABLE IF EXISTS logs CASCADE;
-CREATE TABLE logs
-(
-    id      SERIAL PRIMARY KEY NOT NULL
 );
 
 DROP TABLE IF EXISTS events CASCADE;
@@ -92,19 +90,6 @@ CREATE TABLE events
     id              SERIAL PRIMARY KEY NOT NULL,
     date            DATE,
     description     VARCHAR(100),
-    log_id          INTEGER REFERENCES logs (id) ON DELETE CASCADE
+    order_id        INTEGER REFERENCES orders(id) ON DELETE CASCADE
 );
 
-DROP TABLE IF EXISTS orders CASCADE;
-DROP TYPE IF EXISTS orderStatus;
-CREATE TYPE orderStatus AS ENUM('CHECKED', 'PAID', 'CONFIRMED', 'SHIPPED');
-CREATE TABLE orders
-(
-    id              SERIAL PRIMARY KEY NOT NULL,
-    name            VARCHAR(50),
-    description     VARCHAR(100),
-    cart_id         INTEGER REFERENCES carts (id) ON DELETE SET NULL,
-    user_id         INTEGER REFERENCES users (id) ON DELETE CASCADE,
-    order_status    orderStatus,
-    log_id          INTEGER REFERENCES logs (id) UNIQUE
-);
