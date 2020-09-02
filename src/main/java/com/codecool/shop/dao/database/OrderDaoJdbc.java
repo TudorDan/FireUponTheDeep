2acf9ab9c5b191e30be3a8a405e4cf126c82b77a
TudorDan <tudor.ist@gmail.com>
2020-09-02 23:46:24 +0300

    Code function getOrdersOf(user) [project complete - everything working! = ready for testing]

--- src/main/java/com/codecool/shop/dao/database/OrderDaoJdbc.java
+++ src/main/java/com/codecool/shop/dao/database/OrderDaoJdbc.java
@@ -1,12 +1,10 @@
 package com.codecool.shop.dao.database;
 
 import com.codecool.shop.dao.OrderDao;
-import com.codecool.shop.model.Item;
-import com.codecool.shop.model.Order;
-import com.codecool.shop.model.OrderStatus;
-import com.codecool.shop.model.User;
+import com.codecool.shop.model.*;
 
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class OrderDaoJdbc implements OrderDao {
@@ -88,8 +86,72 @@ public class OrderDaoJdbc implements OrderDao {
 
     @Override
     public List<Order> getOrdersOf(User user) {
-        // TODO: 18.08.2020 Order getOrdersOf(user)
-        return null;
+        List<Order> orders = new ArrayList<>();
+        PreparedStatement st;
+        try (Connection conn = databaseManager.getConnection()) {
+            //get user orders
+            String ordersQuery = "SELECT id, name, date, order_status " +
+                    "FROM orders " +
+                    "WHERE user_id = ? " +
+                    "AND is_my_cart = false";
+
+            st = conn.prepareStatement(ordersQuery);
+            st.setInt(1, user.getId());
+
+            ResultSet ordersResult = st.executeQuery();
+            while( ordersResult.next() ) {
+                //get order data
+                int orderId = ordersResult.getInt("id");
+                String name = ordersResult.getString("name");
+                java.util.Date date = new java.util.Date(ordersResult.getDate("date").getTime());
+                OrderStatus status = OrderStatus.valueOf(ordersResult.getString("order_status"));
+
+                //get order items
+                String itemsQuery = "SELECT itm.quantity AS itmQuantity, " +
+                        "  pri.sum AS priSum, pri.currency AS priCurrency, pri.date AS priDate, " +
+                        "  pro.id AS proId, pro.name AS proName, pro.description AS proDesc, pro.image_file_name AS proImage " +
+                        "FROM orders ord, items itm, prices pri, products pro " +
+                        "WHERE ord.id = ? " +
+                        "  AND itm.order_id = ord.id " +
+                        "  AND itm.price_id = pri.id " +
+                        "  AND pri.product_id = pro.id";
+
+                st = conn.prepareStatement(itemsQuery);
+                st.setInt(1, orderId);
+
+                ResultSet itemsResult = st.executeQuery();
+                List<Item> items = new ArrayList<>();
+                while (itemsResult.next()) {
+                    //create price pri.sum, pri.currency, pri.date
+                    double sum = itemsResult.getDouble("priSum");
+                    String currency = itemsResult.getString("priCurrency");
+                    java.util.Date priDate = itemsResult.getDate("priDate");
+                    Price price = new Price(sum, currency, priDate);
+                    List<Price> prices = new ArrayList<>();
+                    prices.add(price);
+
+                    //create product
+                    int proId = itemsResult.getInt("proId");
+                    String proName = itemsResult.getString("proName");
+                    String proDesc = itemsResult.getString("proDesc");
+                    String proImg = itemsResult.getString("proImage");
+                    Product product = new Product(proId, proName, proDesc, proImg, prices, null, null);
+
+                    //create item and add to list
+                    int quantity = itemsResult.getInt("itmQuantity");
+                    items.add(new Item(product, price, quantity));
+                }
+
+                //create order and add to list
+                Cart cart = new Cart(items);
+                orders.add( new Order(orderId, name, date, status, cart) );
+            }
+            st.close();
+        } catch (SQLException exception) {
+            System.err.println("ERROR: Get orders of user error => " + exception.getMessage());
+        }
+
+        return orders;
     }
     
     public List<Item> getItemsByOrderId() {
