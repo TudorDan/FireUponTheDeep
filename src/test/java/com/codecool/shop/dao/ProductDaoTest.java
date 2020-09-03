af5f189b2369200d1e0440625f662996a1f07fa6
TudorDan <tudor.ist@gmail.com>
2020-09-03 13:16:16 +0300

    Add testAddProduct

--- src/test/java/com/codecool/shop/dao/ProductDaoTest.java
+++ src/test/java/com/codecool/shop/dao/ProductDaoTest.java
@@ -1,9 +1,14 @@
 package com.codecool.shop.dao;
 
 import com.codecool.shop.Tester;
+import com.codecool.shop.model.Category;
+import com.codecool.shop.model.Product;
+import com.codecool.shop.model.Supplier;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import java.util.Date;
 
 class ProductDaoTest {
     private static DataStore dataStore;
@@ -15,8 +20,18 @@ class ProductDaoTest {
     }
 
     @Test
-    void add() {
-        Assertions.fail();
+    void testAddProduct() {
+        Date currentDate = new Date();
+        Supplier prada = new Supplier("Prada", "Luxury fashion house, specializing in leather handbags, travel " +
+                "accessories, shoes, ready-to-wear, perfumes ");
+        dataStore.supplierDao.add(prada);
+        Category ring = new Category("Ring", "Jewelry", "Circular band, often set with gems, for wearing as an " +
+                "ornament");
+        dataStore.categoryDao.add(ring);
+        Product product1 = new Product("Prada Sapphire and diamonds Rings", 250 , "USD", currentDate, "24 karate " +
+                "white gold rings with sapphire and diamonds", "pic1.jpg", ring, prada);
+        dataStore.productDao.add(product1);
+        Assertions.assertTrue(product1.getId() > 0);
     }
 
     @Test
