c4355aefe5754ade5360037faa70a10897e5631a
TudorDan <tudor.ist@gmail.com>
2020-09-03 10:35:56 +0300

    Add blanc tests for OrderDao

---
+++ src/test/java/com/codecool/shop/dao/OrderDaoTest.java
@@ -0,0 +1,18 @@
+package com.codecool.shop.dao;
+
+import org.junit.jupiter.api.Test;
+
+class OrderDaoTest {
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void setPayed() {
+    }
+
+    @Test
+    void getOrdersOf() {
+    }
+}
