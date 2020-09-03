39601f915792b4e028aed8d6eaeaffb99d1637da
TudorDan <tudor.ist@gmail.com>
2020-09-03 11:48:28 +0300

    Add testAddSupplier (errors)

--- src/test/java/com/codecool/shop/dao/SupplierDaoTest.java
+++ src/test/java/com/codecool/shop/dao/SupplierDaoTest.java
@@ -1,6 +1,7 @@
 package com.codecool.shop.dao;
 
 import com.codecool.shop.Tester;
+import com.codecool.shop.model.Supplier;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -15,8 +16,11 @@ class SupplierDaoTest {
     }
 
     @Test
-    void add() {
-        Assertions.fail();
+    void testAddSupplier() {
+        Supplier prada = new Supplier("Prada", "Luxury fashion house, specializing in leather handbags, travel " +
+                "accessories, shoes, ready-to-wear, perfumes ");
+        dataStore.supplierDao.add(prada);
+        Assertions.assertEquals(1, prada.getId());
     }
 
     @Test
