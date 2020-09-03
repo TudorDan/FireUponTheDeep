88afc8ab2367516b6aafe16ed6fcbc12fa0ac2c8
TudorDan <tudor.ist@gmail.com>
2020-09-03 10:31:16 +0300

    Add blanc tests for ProductDao

---
+++ src/test/java/com/codecool/shop/dao/ProductDaoTest.java
@@ -0,0 +1,39 @@
+package com.codecool.shop.dao;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ProductDaoTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void find() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void getBy() {
+    }
+
+    @Test
+    void testGetBy() {
+    }
+
+    @Test
+    void getCurrentPriceIdByProduct() {
+    }
+}
