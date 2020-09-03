db1f3c3c9d78b112b629440493b59f928278ef17
TudorDan <tudor.ist@gmail.com>
2020-09-03 10:29:59 +0300

    Add blanc tests for SupplierDao

---
+++ src/test/java/com/codecool/shop/dao/SupplierDaoTest.java
@@ -0,0 +1,27 @@
+package com.codecool.shop.dao;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SupplierDaoTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void find() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void getAll() {
+    }
+}
