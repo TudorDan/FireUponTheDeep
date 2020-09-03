9063cbebe7775c53da145d81b262788afb1540dc
TudorDan <tudor.ist@gmail.com>
2020-09-03 13:21:07 +0300

    Add testFindProduct

--- src/test/java/com/codecool/shop/dao/ProductDaoTest.java
+++ src/test/java/com/codecool/shop/dao/ProductDaoTest.java
@@ -35,8 +35,24 @@ class ProductDaoTest {
     }
 
     @Test
-    void find() {
-        Assertions.fail();
+    void testFindProduct() {
+        Date currentDate = new Date();
+        Supplier prada = new Supplier("Prada", "Luxury fashion house, specializing in leather handbags, travel " +
+                "accessories, shoes, ready-to-wear, perfumes ");
+        dataStore.supplierDao.add(prada);
+        Category ring = new Category("Ring", "Jewelry", "Circular band, often set with gems, for wearing as an " +
+                "ornament");
+        dataStore.categoryDao.add(ring);
+        Product product1 = new Product("Prada Sapphire and diamonds Rings", 250 , "USD", currentDate, "24 karate " +
+                "white gold rings with sapphire and diamonds", "pic1.jpg", ring, prada);
+        dataStore.productDao.add(product1);
+        Product dbProduct = dataStore.productDao.find(product1.getId());
+        Assertions.assertAll("Product:",
+                () -> Assertions.assertEquals(product1.getId(), dbProduct.getId()),
+                () -> Assertions.assertEquals(product1.getSupplier().getId(), dbProduct.getSupplier().getId()),
+                () -> Assertions.assertEquals(product1.getName(), dbProduct.getName()),
+                () -> Assertions.assertEquals(product1.getDescription(), dbProduct.getDescription())
+        );
     }
 
     @Test
