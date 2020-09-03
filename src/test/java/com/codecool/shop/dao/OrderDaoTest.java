9ab78748195dd038e9b64d8aabb582c1110c459a
TudorDan <tudor.ist@gmail.com>
2020-09-03 15:15:13 +0300

    Add testAddOrder

--- src/test/java/com/codecool/shop/dao/OrderDaoTest.java
+++ src/test/java/com/codecool/shop/dao/OrderDaoTest.java
@@ -1,6 +1,10 @@
 package com.codecool.shop.dao;
 
 import com.codecool.shop.Tester;
+import com.codecool.shop.model.Cart;
+import com.codecool.shop.model.Order;
+import com.codecool.shop.model.User;
+import com.codecool.shop.model.UserStatus;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -15,8 +19,17 @@ class OrderDaoTest {
     }
 
     @Test
-    void add() {
-        Assertions.fail();
+    void testAddOrder() {
+        //add new user
+        User user = new User("testname", "ordermail@test.com", "testpass", "1234", null, null, UserStatus.SIGNED);
+        dataStore.userDao.add(user);
+
+        //add order for new user
+        Order order = new Order(new Cart(), user);
+        dataStore.orderDao.add(order);
+
+        //check id
+        Assertions.assertTrue(order.getId() > 0);
     }
 
     @Test
