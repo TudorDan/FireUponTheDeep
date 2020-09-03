b928a6b987f85be5c8245a04fca62ed8eda40508
TudorDan <tudor.ist@gmail.com>
2020-09-03 10:32:36 +0300

    Add blanc tests for UserDao

---
+++ src/test/java/com/codecool/shop/dao/UserDaoTest.java
@@ -0,0 +1,30 @@
+package com.codecool.shop.dao;
+
+import org.junit.jupiter.api.Test;
+
+class UserDaoTest {
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void isSignedUp() {
+    }
+
+    @Test
+    void getAuthenticatedUser() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void updateUser() {
+    }
+
+    @Test
+    void updateUserCart() {
+    }
+}
