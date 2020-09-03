0f2b5f8fc27c1caa8ec9742b4b1ab2316730c703
TudorDan <tudor.ist@gmail.com>
2020-09-03 10:16:28 +0300

    Add blanc tests for CategoryDao

---
+++ src/test/java/com/codecool/shop/dao/CategoryDaoTest.java
@@ -0,0 +1,26 @@
+package com.codecool.shop.dao;
+
+import org.junit.jupiter.api.Test;
+
+class CategoryDaoTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void find() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void getAll() {
+    }
+}
