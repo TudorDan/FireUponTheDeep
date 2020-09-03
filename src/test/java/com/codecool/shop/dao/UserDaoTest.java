f073c91d85026637f07cb67c1eddd80792275ce9
TudorDan <tudor.ist@gmail.com>
2020-09-03 14:44:00 +0300

    Add testUpdateUser

--- src/test/java/com/codecool/shop/dao/UserDaoTest.java
+++ src/test/java/com/codecool/shop/dao/UserDaoTest.java
@@ -1,6 +1,7 @@
 package com.codecool.shop.dao;
 
 import com.codecool.shop.Tester;
+import com.codecool.shop.model.Address;
 import com.codecool.shop.model.User;
 import com.codecool.shop.model.UserStatus;
 import org.junit.jupiter.api.Assertions;
@@ -64,8 +65,20 @@ class UserDaoTest {
     }
 
     @Test
-    void updateUser() {
-        Assertions.fail();
+    void testUpdateUser() {
+        //add new user
+        Address address = new Address("a", "a", "a", "a");
+        User user = new User("testname", "testemail@test.com", "testpass", "1234", null, null, UserStatus.SIGNED);
+        dataStore.userDao.add(user);
+
+        dataStore.userDao.updateUser(user, "newname", "newmail", "newpass", "5678", address, address);
+
+        Assertions.assertAll("User update:",
+                () -> Assertions.assertEquals("newname", user.getName()),
+                () -> Assertions.assertEquals("newmail", user.getEmail()),
+                () -> Assertions.assertEquals("newpass", user.getPassword()),
+                () -> Assertions.assertEquals("5678", user.getPhoneNumber())
+        );
     }
 
     @Test
